d12dbfc712a5eb24c5fe16f7a8b0fb6fc75e14ea
Lewis Palacios Gonzales <lpalaciosg@unprg.edu.pe>
2019-12-17 02:16:33 -0500

    bucle_iteracion_04

---
+++ palacios/bucle_iteracion_04.py
@@ -0,0 +1,15 @@
+#Ejercicio iteracion 04
+import os
+sociedad=os.sys.argv[1]
+estado_civil=os.sys.argv[2]
+for estado_civil in sociedad:
+    if  (estado_civil=="s"):
+        print("Soltera")
+    if  (estado_civil=="c"):
+        print("Casada")
+    if  (estado_civil=="v"):
+        print("Viuda")
+    if  (estado_civil=="d"):
+        print("Divorciada")
+    #finif
+#finfor
