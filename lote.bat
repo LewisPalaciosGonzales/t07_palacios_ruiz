38f48398b37581f47c90226ea7d2129f0cc3ff2b
Lewis Palacios Gonzales <lpalaciosg@unprg.edu.pe>
2019-12-17 02:01:53 -0500

    bucle_iteracion_01

---
+++ lote.bat
@@ -0,0 +1,4 @@
+python palacios/bucle_iteracion_01.py "a" "b"
+python palacios/bucle_iteracion_01.py "b" "c"
+python palacios/bucle_iteracion_01.py "c" "d"
+python palacios/bucle_iteracion_01.py "d" "a"
